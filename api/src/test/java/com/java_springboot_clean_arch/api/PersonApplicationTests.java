56d6b8e274689b921a8be0a92c38fd5e1d8254ae
adriana.toni <adriana.toni@gmail.com>
2024-11-29 07:57:16 -0300

    Initial commit

---
+++ api/src/test/java/com/java_springboot_clean_arch/api/PersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_springboot_clean_arch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
